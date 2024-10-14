0dcdd140e5fe6f8b972085ede0ae115756d257c1
lifeym <leonardo_yu@hotmail.com>
2024-10-15 03:07:17 +0800

    WIP

--- flake.nix
+++ flake.nix
@@ -2,21 +2,60 @@
   description = "NixOS configs";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # Official NixOS package source, using nixos's unstable branch by default
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ...}@attrs: {
+  outputs = { self, nixpkgs, nix-darwin, ...}@inputs:
+  let
+    genSpecialArgs = system:
+      inputs
+      // {
+        # use unstable branch for some packages to get the latest updates
+        pkgs-unstable = import inputs.nixpkgs-unstable {
+          inherit system; # refer the `system` parameter form outer scope recursively
+          # To use chrome, we need to allow the installation of non-free software
+          config.allowUnfree = true;
+        };
+        pkgs-stable = import inputs.nixpkgs-stable {
+          inherit system;
+          # To use chrome, we need to allow the installation of non-free software
+          config.allowUnfree = true;
+        };
+      };
+  in {
     nixosConfigurations = {
-      red-chamber-lin-daiyu = let
+      red-yuanchun =
+      let
+        system = "x86_64-linux";
         myargs = {
           username = "lifeym";
         };
       in
         nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = attrs // { inherit myargs; };
+          # system = "x86_64-linux";
+          inherit system;
+          specialArgs = inputs // { inherit myargs; };
           modules = [
-            ./hosts/red-chamber-lin-daiyu
+            ./hosts/nixos/red-yuanchun
+          ];
+        };
+    };
+
+    darwinConfigurations = {
+      sansan =
+      let
+        system = "x86_64-darwin";
+      in
+        nix-darwin.lib.darwinSystem {
+          inherit system;
+          specialArgs = genSpecialArgs system;
+          modules = [
+            ./hosts/nix-darwin/sansan
           ];
         };
     };
---
+++ hosts/nix-darwin/sansan/default.nix
@@ -0,0 +1,136 @@
+{ pkgs, pkgs-stable, lib, ... }:
+
+{
+    # List packages installed in system profile. To search by name, run:
+    # $ nix-env -qaP | grep wget
+    environment.systemPackages = with pkgs; [
+      ###
+
+      # utilities
+      bat
+      chezmoi
+      dua
+      fzf
+      gnumake
+      # see: https://github.com/NixOS/nixpkgs/issues/348309
+      # has not fixed yet(unstable)
+      # go-task
+      hledger
+      htop
+      ncdu
+      neovim
+      nmap
+      nushell
+      p7zip
+      rclone
+      ripgrep
+      termscp
+      thefuck
+      tmux
+      vifm # most confortable vim like file manager
+      vim
+      xz
+      yazi # blazing fast file manager
+      zoxide
+
+      ###
+
+      # vm/container related
+      docker-client
+      qemu
+      colima
+
+      ###
+
+      # develop tools
+      direnv
+      gh
+      git
+      git-credential-manager
+      gitui
+      jq
+      mycli
+      nixd
+      yq
+
+      ###
+
+      # k8s tools
+      argocd
+      k9s
+      kubectl
+      kubeseal
+      kustomize
+      minikube
+      tektoncd-cli
+    ] ++ [
+      # see: https://github.com/NixOS/nixpkgs/issues/348309
+      # has not fixed yet(unstable)
+      # use stable version instead
+      pkgs-stable.go-task
+    ];
+
+    homebrew.enable = true;
+    homebrew.taps = [
+      "homebrew/bundle"
+      # "homebrew/cask-fonts"
+      # "homebrew/cask-versions"
+      "homebrew/services"
+      "wez/wezterm"
+    ];
+
+    homebrew.brews = [
+      "docker-credential-helper"
+    ];
+
+    homebrew.casks = [
+      "dbeaver-community"
+      "google-chrome"
+      "joplin"
+      "mysqlworkbench"
+      "qbittorrent"
+      "qq"
+      "font-sarasa-gothic"
+      "syncthing"
+      "vagrant"
+      "vagrant-vmware-utility"
+      "visual-studio-code"
+      "wezterm"
+    ];
+
+    environment.variables = {
+      EDITOR = "vim";
+      VIFM = "$HOME/.config/vifm";
+    };
+
+    # DO NOT use services.nix-daemon.enabled = true,
+    # if nix was installed by a installer(eg: determinate nix-installer).
+    nix.useDaemon = true;
+    # nix.package = pkgs.nix;
+
+    # Nix linux-builder settings.
+    nix.linux-builder.enable = true;
+
+    # Necessary for using flakes on this system.
+    nix.settings.experimental-features = "nix-command flakes";
+    nix.settings.bash-prompt-prefix = "(nix:$name)\\040";
+    nix.settings.substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
+    nix.settings.trusted-users = [ "@admin" ];
+
+    # Create /etc/zshrc that loads the nix-darwin environment.
+    programs.zsh.enable = true;  # default shell on catalina
+
+    # Set Git commit hash for darwin-version.
+    # system.configurationRevision = self.rev or self.dirtyRev or null;
+
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    system.stateVersion = 5;
+
+    system.keyboard.enableKeyMapping = true;
+    system.keyboard.remapCapsLockToControl = true;
+    system.defaults.dock.show-recents = false;
+    system.defaults.dock.autohide = true;
+
+    security.pam.enableSudoTouchIdAuth = true;
+}
