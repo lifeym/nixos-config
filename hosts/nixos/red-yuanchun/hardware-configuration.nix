a845e6ddd65708579c2e49f90374b8f03b1d31e3
lifeym <leonardo_yu@hotmail.com>
2024-10-17 03:20:35 +0800

    hardware: add filesystem

--- hosts/nixos/red-yuanchun/hardware-configuration.nix
+++ hosts/nixos/red-yuanchun/hardware-configuration.nix
@@ -11,6 +11,21 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/dc13b21c-53b1-4bff-906e-5098f2670c01";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/DCB9-BEAB";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/290f2d49-3bb8-4bc2-ac2e-88a72305a0b4"; }
+    ];
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
