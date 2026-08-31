056f52dfb53975a0fa150283e1ea00cb8ee3d4c0
lifeym <leonardo_yu@hotmail.com>
2026-09-01 00:19:10 +0800

    [red-daiyu] paperless

--- hosts/red-daiyu/srv/paperless.nix
+++ hosts/red-daiyu/srv/paperless.nix
@@ -1,8 +1,19 @@
 { config, pkgs-unstable, ... }:
 {
+  # Nix module(26.05) for paperless has a PAPERLESS_SECRET_KEY bug with v3,
+  # So we cannot run paperless v3 under nixos 26.05 without magic.
+  # There are 3 way to do:
+  # 1. Return to paperless 2.20 under nixos 26.05
+  # 2. Switch from nixos 26.05 to nixos unstable
+  # 3. Replace native nix module with container under nixos 26.05
+  # 4. Do some black magics...
+  # Since the magics are black and dirty enough, I decide to fallback to paperless 2.20...
+  # SeeAlso: https://github.com/NixOS/nixpkgs/blob/nixos-26.05/nixos/modules/services/misc/paperless.nix
   services.paperless = {
     enable = true;
-    environmentFile = config.sops.templates."paperless-secret-key".path;
+    # under nixos 26.05, this module is always use a ${cfg.dataDir}/nixos-paperless-secret-key file.
+    # next line is ready for paperless v3 and nixos 26.05+
+    # environmentFile = config.sops.templates."paperless-secret-key".path;
     consumptionDirIsPublic = true;
     dataDir = "/mnt/data/lib/paperless";
     domain = "paperless.lifeym.xyz";
@@ -14,13 +25,11 @@
         "desktop.ini"
       ];
       PAPERLESS_OCR_LANGUAGE = "chi_sim+eng";
-      PAPERLESS_OCR_LANGUAGES = [ "chi_sim" "eng" "chi_tra" "jpn" ];
+      PAPERLESS_OCR_LANGUAGES = [ "chi-sim" "eng" "chi-tra" "jpn" ];
       PAPERLESS_OCR_USER_ARGS = {
         optimize = 1;
         pdfa_image_compression = "lossless";
       };
     };
-
-    package = pkgs-unstable.paperless-ngx;
   };
 }
